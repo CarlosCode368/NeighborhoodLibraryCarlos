2a0d09f8c6e57a2ecf092365a91effafffc991ba
CarlosCode368 <carlossarabia_r@hotmail.com>
2025-04-18 13:27:37 -0700

    NL1

--- src/Book.java
+++ src/Book.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class Book {
+    int id;
+    String isbn;
+    String title ;
+    boolean isCheckOut ;
+    String checkOutTo;
+    Book(int id, String isbn, String title){
+        this.id=id;
+        this.isbn=isbn;
+        this.title=title;
+        this.isCheckOut=false;
+        this.checkOutTo="";
+
+    }
+    void checkOut(String name){
+        this.checkOutTo=name;
+        this.isCheckOut=true;
+    }
+    void checkIn(){
+        this.checkOutTo="";
+        this.isCheckOut=false;
+    }
+    //TODO ADD GETTERS AND SETTERS?
 }
